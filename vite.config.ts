41d09dc185e1fc9e053adf39c43ddf35587eca94
kotatsuyarn <121517360+kotatsuyarn@users.noreply.github.com>
2023-04-11 23:40:53 +0200

    feat(unocss): add unocss into vite as a plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    UnoCSS({
+      mode: 'vue-scoped',
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
