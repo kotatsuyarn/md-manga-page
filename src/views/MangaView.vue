420a2754ac601c5daa8eb9e66ef2e7cf0c3485fc
kotatsuyarn <121517360+kotatsuyarn@users.noreply.github.com>
2023-04-11 23:56:38 +0200

    feat(MangaView): add MangaView

---
+++ src/views/MangaView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div class="flex flex-col gap-2 w-8/12">
+      <div class="flex flex-col gap-4 border border-solid border-gray-300">
+        <div>
+          <div class="flex gap-2">
+            <div class="w-[200px] h-[280px] bg-gray-300"></div>
+
+            <div class="flex flex-col justify-between">
+              <div class="flex flex-col gap-2">
+                <span class="text-2xl">Title</span>
+                <span class="text-lg">Subtitle</span>
+              </div>
+
+              <div class="flex flex-col gap-2">
+                <div>Author</div>
+
+                <div class="flex gap-2 items-center justify-between">
+                  <button>Add to library</button>
+                  <button>Start reading</button>
+                  <button>Report</button>
+                  <button>Upload Chapter</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex gap-2">
+            <div class="w-[200px]"></div>
+
+            <div>
+              <div class="flex gap-2 items-center justify-between">
+                <span>Genre 1</span>
+                <span>Genre 2</span>
+                <span>Genre 3</span>
+                <span>Publication</span>
+                <span>Status</span>
+              </div>
+              <div>Statistics</div>
+            </div>
+          </div>
+        </div>
+
+        <div>Description</div>
+      </div>
+
+      <div class="flex flex-col gap-2 w-full border border-solid border-gray-300">
+        <div class="border border-solid border-gray-300">
+          <button>Chapters</button>
+          <button>Comments</button>
+          <button>Art</button>
+          <button>Related</button>
+        </div>
+
+        <div class="flex">
+          <div class="w-3/12">Information</div>
+
+          <div class="flex flex-col gap-2 w-9/12">
+            <div class="flex items-center justify-between">
+              <button>Descending</button>
+              <button>Collapse</button>
+            </div>
+
+            <div>Chapters</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
