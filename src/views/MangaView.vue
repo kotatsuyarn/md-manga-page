5d421c2b9d87eb4da66ab334785eafc13896e8f9
kotatsuyarn <121517360+kotatsuyarn@users.noreply.github.com>
2023-04-12 20:52:46 +0200

    feat(MangaView): use MDTabBar and apply MDButton changes

--- src/views/MangaView.vue
+++ src/views/MangaView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import MDBadge from '@/components/md/MDBadge.vue';
 import MDButton from '@/components/md/MDButton.vue';
+import MDTabBar from '@/components/md/MDTabBar.vue';
 </script>
 
 <template>
@@ -8,8 +9,8 @@ import MDButton from '@/components/md/MDButton.vue';
     <div class="flex flex-col gap-2 w-8/12">
       <div class="flex flex-col gap-4 border border-solid border-gray-300">
         <div class="flex flex-col gap-4">
-          <div class="flex gap-2">
-            <div class="w-[200px] h-[280px] bg-gray-300"></div>
+          <div class="flex gap-6">
+            <div class="w-[200px] h-[280px] bg-true-gray-300 rounded"></div>
 
             <div class="flex flex-col justify-between">
               <div class="flex flex-col gap-2">
@@ -21,10 +22,10 @@ import MDButton from '@/components/md/MDButton.vue';
                 <div>Author</div>
 
                 <div class="flex gap-2 items-center justify-between">
-                  <MDButton button-type="primary">Add to library</MDButton>
-                  <MDButton>Start reading</MDButton>
-                  <MDButton>Report</MDButton>
-                  <MDButton>Upload Chapter</MDButton>
+                  <MDButton button-type="primary" rounded>Add to library</MDButton>
+                  <MDButton rounded>Start reading</MDButton>
+                  <MDButton rounded>Report</MDButton>
+                  <MDButton rounded>Upload Chapter</MDButton>
                 </div>
               </div>
             </div>
@@ -63,25 +64,34 @@ import MDButton from '@/components/md/MDButton.vue';
       </div>
 
       <div class="flex flex-col gap-2 w-full border border-solid border-gray-300">
-        <div class="border border-solid border-gray-300">
-          <button>Chapters</button>
-          <button>Comments</button>
-          <button>Art</button>
-          <button>Related</button>
-        </div>
+        <MDTabBar :tab-names="['Chapters', 'Comments', 'Art', 'Related']">
+          <template v-slot:chapters>
+            <div class="flex">
+              <div class="w-3/12">Information</div>
 
-        <div class="flex">
-          <div class="w-3/12">Information</div>
+              <div class="flex flex-col gap-2 w-9/12">
+                <div class="flex items-center justify-between">
+                  <button>Descending</button>
+                  <button>Collapse</button>
+                </div>
 
-          <div class="flex flex-col gap-2 w-9/12">
-            <div class="flex items-center justify-between">
-              <button>Descending</button>
-              <button>Collapse</button>
+                <div>Chapters</div>
+              </div>
             </div>
+          </template>
 
-            <div>Chapters</div>
-          </div>
-        </div>
+          <template v-slot:comments>
+            <div>Comments</div>
+          </template>
+
+          <template v-slot:art>
+            <div>Art</div>
+          </template>
+
+          <template v-slot:related>
+            <div>Related</div>
+          </template>
+        </MDTabBar>
       </div>
     </div>
   </div>
