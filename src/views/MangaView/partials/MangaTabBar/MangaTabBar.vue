42969ec4ef9c17f411ec1f8f5ef8c6cb2fcefe4d
kotatsuyarn <121517360+kotatsuyarn@users.noreply.github.com>
2023-04-16 12:50:09 +0200

    feat(MangaTabBar): add text base

--- src/views/MangaView/partials/MangaTabBar/MangaTabBar.vue
+++ src/views/MangaView/partials/MangaTabBar/MangaTabBar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import MDTabBar from '@/components/md/MDTabBar.vue';
-import MangaTabChapters from './partials/MangaTabChapters.vue';
+import MangaTabChapters from './partials/MangaTabChapters/MangaTabChapters.vue';
 import MangaTabComments from './partials/MangaTabComments.vue';
 import MangaTabArt from './partials/MangaTabArt.vue';
 import MangaTabRelated from './partials/MangaTabRelated.vue';
@@ -9,6 +9,10 @@ import MangaTabRelated from './partials/MangaTabRelated.vue';
 <template>
   <div class="flex flex-col gap-2 w-full border border-solid border-gray-300">
     <MDTabBar :tab-names="['Chapters', 'Comments', 'Art', 'Related']">
+      <template v-slot:tabName="{ tabName }">
+        <span class="text-base">{{ tabName }}</span>
+      </template>
+
       <template v-slot:chapters>
         <MangaTabChapters />
       </template>
